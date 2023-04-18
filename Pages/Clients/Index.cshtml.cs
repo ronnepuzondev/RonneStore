17accd800ae35775178710206d4f9fbb32281e7b
RonnePuzonDev <ronnepdev@gmail.com>
2023-04-18 15:47:20 -0700

    add client list

--- Pages/Clients/Index.cshtml.cs
+++ Pages/Clients/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace RonneStore.Pages.Clients
 {
@@ -10,10 +11,35 @@ namespace RonneStore.Pages.Clients
         {
             try
             {
-                String connectionString = "";
+                String connectionString = "Data Source=.\\sqlexpress;Initial Catalog=mystore;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM clients";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read()) 
+                            {
+                                ClientInfo clientInfo = new ClientInfo();
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
+                                clientInfo.created_at = reader.GetDateTime(5).ToString();
+
+                                listClients.Add(clientInfo);
+                            }
+                        }
+                    }
+                }
             }
             catch (Exception ex) 
             { 
+                Console.WriteLine("Exception: " + ex.ToString());
             }
         }
     }
@@ -25,5 +51,6 @@ namespace RonneStore.Pages.Clients
         public String email;
         public String phone;
         public String address;
+        public String created_at;
     }
 }
